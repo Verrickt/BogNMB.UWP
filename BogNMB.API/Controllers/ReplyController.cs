b155c0f926dec2d52a64f2885d80837c317eb1b9
Verrickt <Adios_verrickt@hotmail.com>
2020-04-19 21:05:24 +0800

    Add reply controller

---
+++ BogNMB.API/Controllers/ReplyController.cs
@@ -0,0 +1,24 @@
+﻿using BogNMB.API.POCOs;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BogNMB.API.Controllers
+{
+    public class ReplyController : ControllerBase
+    {
+        public ReplyController(ApiConfig config) : base(config)
+        {
+        }
+
+        public override string ApiMode => "r";
+
+        public async Task<Reply> GetReplyAsync(string id)
+        {
+            var json = await GetAsync(id, "0");
+            return JsonConvert.DeserializeObject<Reply>(json);
+        }
+    }
+}
---
+++ BogNMB.API/POCOs/Reply.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BogNMB.API.POCOs
+{
+    public class Reply
+    {
+        public string No { get; set; }
+        public int Res { get; set; }
+        public string Title { get; set; }
+        public string Name { get; set; }
+        public string Com { get; set; }
+        public string Time { get; set; }
+        public string Id { get; set; }
+        public string Img { get; set; }
+        public string Src { get; set; }
+        public string Ext { get; set; }
+        public int @Lock { get; set; }
+        public int Htmb { get; set; }
+        public int Sage { get; set; }
+        public int Admin { get; set; }
+    }
+}
