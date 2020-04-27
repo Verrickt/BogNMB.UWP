98623e9c638b38c01cc77f223dc3369b5badc274
Verrickt <Adios_verrickt@hotmail.com>
2020-04-27 23:06:09 +0800

    Add cache for forum list

--- BogNMB.UWP/ViewModel/MainViewModel.cs
+++ BogNMB.UWP/ViewModel/MainViewModel.cs
@@ -1,8 +1,12 @@
 ﻿using BogNMB.API.Controllers;
+using BogNMB.API.POCOs;
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using Microsoft.Toolkit.Uwp.Helpers;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +14,8 @@ namespace BogNMB.UWP.ViewModel
 {
     public class MainViewModel : ViewModelBase
     {
+        private readonly LocalObjectStorageHelper _storageHelper=new LocalObjectStorageHelper();
+        private const string forum_cache_key = "forums";
         private ObservableCollection<ForumViewModel> _forums;
         public ObservableCollection<ForumViewModel> Forums
         {
@@ -23,14 +29,34 @@ namespace BogNMB.UWP.ViewModel
             }
         }
 
+        public RelayCommand RefreshForumsCommand { get;private set; }
+
         public MainViewModel()
         {
             Forums = new ObservableCollection<ForumViewModel>();
+            RefreshForumsCommand = new RelayCommand(async() =>
+            {
+                var pocos = await new ForumController(Config.ApiConfig).GetForumsAsync();
+                await _storageHelper.SaveFileAsync(forum_cache_key, pocos);
+                _forums.Clear();
+                pocos.ForEach(i => _forums.Add(new ForumViewModel(i)));
+                SelectedForum = Forums.FirstOrDefault();
+            });
         }
         public async Task InitializeAsync()
         {
-            var pocos = await new ForumController(Config.ApiConfig).GetForumsAsync();
+            List<Forum> pocos;
+            if(await _storageHelper.FileExistsAsync(forum_cache_key))
+            {
+                pocos = await _storageHelper.ReadFileAsync<List<Forum>>(forum_cache_key);
+            }
+            else
+            {
+                pocos = await new ForumController(Config.ApiConfig).GetForumsAsync();
+                await _storageHelper.SaveFileAsync(forum_cache_key, pocos);
+            }
             pocos.ForEach(i => _forums.Add(new ForumViewModel(i)));
+            SelectedForum = Forums.FirstOrDefault();
         }
 
         private ForumViewModel _selectedForum;
