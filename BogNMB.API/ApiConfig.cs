bc9249f8888831a9e19bbc95cf8b76e3190ef801
Verrickt <Adios_verrickt@hotmail.com>
2020-04-28 00:01:01 +0800

    Allow construction from outside

--- BogNMB.API/ApiConfig.cs
+++ BogNMB.API/ApiConfig.cs
@@ -7,10 +7,20 @@ namespace BogNMB.API
 	{
 		public string BaseUrl { get; set; } = "http://bog.ac/api/";
 		public string DefaultUA { get; set; } = $"BogNMB.NetCore.API/{Assembly.GetExecutingAssembly().GetName().Version}";
-		private ApiConfig()
-		{
 
+		public static bool operator ==(ApiConfig c1, ApiConfig c2) => c1.Equals(c2);
+		public static bool operator !=(ApiConfig c1, ApiConfig c2) => !(c1 == c2);
+		public override int GetHashCode()
+		{
+			return BaseUrl.GetHashCode();
 		}
+
+		public override bool Equals(object obj)
+		{
+			if (obj is ApiConfig config) return config.BaseUrl == this.BaseUrl;
+			return false;
+		}
+
 		public static ApiConfig Default => new ApiConfig() { BaseUrl = "http://bog.ac/api/" };
 		public static ApiConfig Test => new ApiConfig() { BaseUrl = "http://test.bog.ac/api/" };
 	}
